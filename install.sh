3ffc7be20dac71a84ae8b658f85f09ec434555fc
Fajar Firdaus <35714197+FajarTheGGman@users.noreply.github.com>
2019-07-12 17:52:44 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 apt-get update && apt-get upgrade -y
 apt-get install nodejs -y
-npm install inquirer shelljs chalk log-symbols ora jsome colors
+npm install inquirer shelljs chalk log-symbols ora jsome colors 
+npm install -g bytenode
 ./run_linux.sh
